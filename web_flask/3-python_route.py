a1af18d76055d67d96124c98c6f53f014fc6498b
Ahmed Rady <rady20991@gmail.com>
2024-07-29 04:02:20 +0300

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """function return hello message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """function return text"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """replace text with variable"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """replace text with variable"""
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
